cf8f7934cd6f6e5763468647f2cef8570b83fe66
Mike S <michael.schendel@hacksurfer.com>
2014-02-17 22:28:01 -0500

    encoding binary fully operational, and decoding fully operational

--- src/binaryEncoding.py
+++ src/binaryEncoding.py
@@ -6,19 +6,18 @@ def encode(to_encode):
     #get first 8 chars encode as a byte
     if(len(to_encode) % 8 != 0):
        to_encode = to_encode + (8-(len(to_encode) % 8)) * '0'
-    print(to_encode)
+    #print(to_encode)
     bytes = []
     my_range = int(len(to_encode)/8)
     for j in range (my_range):
         encoded_int = 0
-        for i in range(0,7):
-            print((j*8)+i)
+        for i in range(0,8):
+            #print((j*8)+i)
             char = to_encode[((j)*8)+i]
             if(char == '1'):
-                ###TODO this contains an error
                 encoded_int = encoded_int | (1<<(7-i))
         bytes.insert(j, (chr(encoded_int)))
-        print(bin(encoded_int))
+        #print(bin(encoded_int))
 
     return ''.join(bytes)
 
@@ -31,8 +30,9 @@ def decode(to_decode):
         int_to_decode = ord(i)
         truncatedBin = ("{0:b}".format(int_to_decode))
         str = ((8-len(truncatedBin)) * '0') + truncatedBin
-        print(str)
+        #print(str)
         decoded_str = decoded_str + str
 
 
     return decoded_str
+
